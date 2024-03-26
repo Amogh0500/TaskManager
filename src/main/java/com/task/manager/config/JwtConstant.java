e3d0bb9153aca30fbb31e02f13486daaa72d7b2a
amoghburte <amoghburte05@gmail.com>
2024-03-26 22:23:32 +0530

    first commit

---
+++ src/main/java/com/task/manager/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.task.manager.config;
+
+public class JwtConstant {
+
+	public static final String JWT_SECRET = "lsfjalkfjaskfjaslfafnamafkjalfjaskfljlfjaldseoi";
+
+	public static final String JWT_HEADER = "Authorization";
+
+}
